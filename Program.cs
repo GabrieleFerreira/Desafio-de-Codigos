2b0a6dbf16975c4d116d530ba9d00159ac81cc9a
Gabriele <gabriele.ferreira@ufvjm.edu.br>
2022-04-12 21:18:11 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+ 
+System.Console.WriteLine("Digite o valor do saque:");
+ int valor = int.Parse(System.Console.ReadLine());
+int [] Notas = new int [7]  {100,50,20,10,5,2,1};
+int total = 0;
+
+
+
+for (int i =0 ; i< Notas.Length; i++){
+    total = valor / Notas[i];
+    System.Console.WriteLine($"{total} nota(s) de {Notas[i]},00 real(is)");
+    valor %= Notas[i];
+}
